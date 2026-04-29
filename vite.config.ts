31b08f58af16617bd7f6d9c88e0889210b50f768
Isabely Perez Marques <isabely.pmarques.mult@gmail.com>
2026-04-29 18:40:22 -0300

    fix: base server

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-base: "/Beelivery/",
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  // O nome do repositório deve estar entre barras
+  base: "/Beelivery/", 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
-}));
+});
