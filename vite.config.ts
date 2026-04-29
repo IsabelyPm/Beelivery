02e8ab5c6fb891a8ecfecd20791edc0be247b6f5
Isabely Perez Marques <isabely.pmarques.mult@gmail.com>
2026-04-29 18:51:13 -0300

    fix: base vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // O nome do repositório deve estar entre barras
-  base: "/Beelivery/", 
+  
+   // base: "/Beelivery/", 
   plugins: [react()],
   resolve: {
     alias: {
